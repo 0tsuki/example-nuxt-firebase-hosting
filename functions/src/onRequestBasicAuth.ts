768ba61dd2e330a1d15675f57531cef02a6819ce
OTSUKI Shinsuke <otsuki.shinsuke@gmail.com>
2019-10-29 15:33:37 +0900

    build: deploy

--- functions/src/onRequestBasicAuth.ts
+++ functions/src/onRequestBasicAuth.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 const auth = require('basic-auth-connect');
 
 const app = express();
-app.use(auth(process.env.basicAuth.id, process.env.basicAuth.password));
-app.use(express.static(__dirname + '/../../dist'));
+app.use(auth(functions.config().basic_auth.id, functions.config().basic_auth.password));
+app.use(express.static(__dirname + '/../dist'));
 
 export const onRequestBasicAuth = functions.https.onRequest(app);
